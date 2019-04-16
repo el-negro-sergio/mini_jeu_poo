9166d0d14863910e5ddd484c242584e03bf2fb5f
Rodrigo Vallejos <vallejosrs@gmail.com>
2019-04-17 01:29:56 +0200

    Update 02

--- app_01.rb
+++ app_01.rb
@@ -8,12 +8,12 @@ player1 = Player.new("Josiane")
 player2 = Player.new("José")
 
 while player1.life_points > 0 && player2.life_points > 0 do
-  puts 'Here is the state of each player'
+  puts "\n\nHere is the state of each player:\n\n"
   puts player1.show_state
   puts player2.show_state
 
 
-  puts "Lets move to the attack phase"
+  puts "\n\nLets move to the attack phase:\n\n"
   puts player1.attacks(player2)
   if player2.life_points <= 0
     break
